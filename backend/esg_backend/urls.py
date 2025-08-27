6552a953e000bc7b993fae4159c08a26e3df756d
AAASSEM <Abdullah.Ahmed.22230600@commerce.helwan.edu.eg>
2025-08-27 09:03:55 +0300

    Update URLs and staticfiles

--- backend/esg_backend/urls.py
+++ backend/esg_backend/urls.py
@@ -2,9 +2,13 @@
 URL configuration for esg_backend project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+from django.views.static import serve
+import os
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
@@ -14,3 +18,16 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # In production, serve React build files
+    # Serve static files for React build
+    urlpatterns += [
+        re_path(r'^static/(?P<path>.*)$', serve, {
+            'document_root': os.path.join(settings.BASE_DIR, 'staticfiles'),
+        }),
+    ]
+
+# Catch-all pattern for React routing (must be last)
+urlpatterns += [
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='react_app'),
+]
